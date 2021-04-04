9ed36653af552d5f6b762c53567a0146d73d9a08
Jonathan Sands <jonathan.sands@edu.devinci.fr>
2021-04-04 22:17:09 +0200

    api endpoints done

---
+++ server/routes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router(); 
+const bdd = require('./db');
+
+
+router.get('/products/search', async (req, res)=>{
+
+    let brand = req.query.brand?{brand:req.query.brand}:{}
+    let categorie = req.query.categorie?{categorie:req.query.categorie}:{}
+    let limit = parseInt(req.query.limit)?parseInt(req.query.limit):12
+    let price = parseFloat(req.query.price)?{price:parseFloat(req.query.price)}:{}
+
+    let result = await bdd.getFilteredProduct(limit, brand, price, categorie)
+
+    res.send({
+        limit,
+        brand:brand.brand,
+        price:price.price,
+        TotalNumberOfProducts:result.n,
+        results:result.res
+
+    })
+})
+
+
+
+router.get('/products/:id',  async (req, res)=>{
+    res.send(await bdd.getProductById(req.params.id))
+
+})
+
+
+module.exports = router; 
